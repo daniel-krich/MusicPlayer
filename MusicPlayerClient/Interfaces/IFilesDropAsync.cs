4085c9991c1ce86c45407ddbacccc50089178d77
zogov123 <danielkrich147@gmail.com>
2022-06-29 03:19:20 +0300

    rename

---
+++ MusicPlayerClient/Interfaces/IFilesDropAsync.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerClient.Interfaces
+{
+    public interface IFilesDropAsync
+    {
+        Task OnFilesDroppedAsync(string[] files);
+    }
+}
